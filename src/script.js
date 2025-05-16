9c168c7d7340e4dffa0f6f4031290532c5483dd5
acejebas <xdv52966@gmail.com>
2025-05-16 17:15:26 -0300

    feat: javasc part1

---
+++ src/script.js
@@ -0,0 +1,6 @@
+function Zoom () {
+    const Fotos=document.getElementById("imagem1").classList.add="zoom";
+    const test=document.getElementById("imagem1").classList;
+    console.log(test)
+    
+}
